b8bd9481438faee514420413cd116b4297a1f7d7
mrmoneyc <moneyc.net@gmail.com>
2017-12-15 14:32:49 +0800

    test commit

--- src/github.com/screwdriver-cd-test/quickstart-golang/hello.go
+++ src/github.com/screwdriver-cd-test/quickstart-golang/hello.go
@@ -3,3 +3,7 @@ package hello
 func greetings() string {
 	return "Hello, Go"
 }
+
+func echo(s string) string {
+	return s
+}
--- src/github.com/screwdriver-cd-test/quickstart-golang/hello_test.go
+++ src/github.com/screwdriver-cd-test/quickstart-golang/hello_test.go
@@ -6,8 +6,17 @@ import (
 )
 
 func TestGreetings(t *testing.T) {
-    expected := "Hello, Go"
+	expected := "Hello, Go"
 	result := greetings()
+
+	if !strings.EqualFold(result, expected) {
+		t.Errorf("result == %q, want %q", result, expected)
+	}
+}
+
+func TestMain(t *testing.T) {
+	expected := "Hi, this is test"
+	result := echo(expected)
 
 	if !strings.EqualFold(result, expected) {
 		t.Errorf("result == %q, want %q", result, expected)
